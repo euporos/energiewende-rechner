746a37c13cbef1394630145c571d53afb33d360e
Oliver Motz <services@olivermotz.com>
2021-06-05 23:45:09 +0200

    Add automatic prerendering

---
+++ src/prerender/main.cljs
@@ -0,0 +1,31 @@
+(ns prerender.main
+  (:require
+   ["puppeteer" :as puppeteer]
+   [clojure.string :as str]
+   ["path" :as path]
+   ;; ["fs" :as fs]
+   ;; [plibs.phtmltohiccup :as hth]
+   [clojure.core.async :refer [go <!]]
+   [cljs.core.async.interop :refer-macros [<p!]]))
+
+
+(def app-page (str "file://"
+                   (.resolve path "resources/public/index.html")))
+
+(defn prerender
+  ""
+  []
+  (go
+    (let [browser (<p! (.launch puppeteer))
+          page (<p! (.newPage browser))]
+      (try
+        (<p! (.goto page app-page))
+        (catch js/Error err (js/console.log (ex-cause err))))
+      (let [html (<p! (.content page))]
+        ;; (spit "content.html" html)
+        (print
+         (some
+          #(if (re-find #"id=\"app\"" %) %)
+          (str/split 
+           html "\n"))))
+      (.close browser))))
