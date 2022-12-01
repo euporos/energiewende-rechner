ee0b1adce84d80917f1c76dc438ae7e42bea811e
Oliver Motz <services@olivermotz.com>
2022-12-01 22:03:20 +0100

    rename

--- resources/snippets/preview-image.php
+++ resources/snippets/preview-image.php
@@ -2,24 +2,24 @@
 $qstring = $_SERVER['QUERY_STRING'];
 
 if (empty($qstring)) {
-    $og_img_link = "{{settings.main-site}}{{settings.default-preview-image}}";
+    $dynamic_img_link = "{{settings.main-site}}{{settings.default-preview-image}}";
 } else {
     parse_str ( parse_url ( '?'.$qstring , PHP_URL_QUERY ) , $params );
     $savestate = $params["s"];
     $savestate_img_filename = "previews/".$savestate.".png";
     $savestate_img_filename_absolute = dirname(__FILE__) . "/" . $savestate_img_filename;
 
-    $og_img_link = "{{settings.preview-api}}?".$qstring;
+    $dynamic_img_link = "{{settings.preview-api}}?".$qstring;
 
     if (! file_exists($savestate_img_filename_absolute)) {
-        file_put_contents($savestate_img_filename, file_get_contents($og_img_link));
-        echo $og_img_link;
+        file_put_contents($savestate_img_filename, file_get_contents($dynamic_img_link));
+        echo $dynamic_img_link;
     }
     else {
         echo $savestate_img_filename;
     }
 
-    echo $savestate_img_filename_absolute;
+    echo file_exists($savestate_img_filename_absolute);
 
 }
 ?>
