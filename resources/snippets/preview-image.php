f022f7bbd1d58bac2ae61618e01c95a9ad19ec9b
Oliver Motz <services@olivermotz.com>
2022-12-01 22:11:56 +0100

    Inrement

--- resources/snippets/preview-image.php
+++ resources/snippets/preview-image.php
@@ -2,7 +2,7 @@
 $qstring = $_SERVER['QUERY_STRING'];
 
 if (empty($qstring)) {
-    $dynamic_img_link = "{{settings.main-site}}{{settings.default-preview-image}}";
+    $og_img_link = "{{settings.main-site}}{{settings.default-preview-image}}";
 } else {
     parse_str ( parse_url ( '?'.$qstring , PHP_URL_QUERY ) , $params );
     $savestate = $params["s"];
@@ -13,13 +13,11 @@ if (empty($qstring)) {
 
     if (! file_exists($savestate_img_filename_absolute)) {
         file_put_contents($savestate_img_filename, file_get_contents($dynamic_img_link));
-        echo $dynamic_img_link;
+        $og_img_link = $dynamic_img_link;
     }
     else {
-        echo $savestate_img_filename;
+        $og_img_link = $savestate_img_filename;
     }
-
-    echo file_exists($savestate_img_filename_absolute);
 
 }
 ?>
