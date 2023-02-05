5f5f1b6724169b00fc2f2aa6d4e140f1f00de0dd
Oliver Motz <services@olivermotz.com>
2023-02-05 20:33:05 +0100

    Fix error in case of empty query string

--- resources/snippets/generate_vars.php
+++ resources/snippets/generate_vars.php
@@ -1,12 +1,13 @@
 <?php
 
-$bare_query_string = $_SERVER['QUERY_STRING'];
-$full_query_string = "?".$bare_query_string;
-$full_url = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$query_string_exists = array_key_exists("QUERY_STRING", $_SERVER);
 
-if (empty($bare_query_string)) {
+if (! $query_string_exists or empty($_SERVER['QUERY_STRING'])) {
     $og_img_link = "{{settings.main-site}}{{settings.default-preview-image}}";
 } else {
+    $bare_query_string = $_SERVER['QUERY_STRING'];
+    $full_query_string = "?".$bare_query_string;
+    $full_url = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     parse_str ( parse_url ( '?'.$bare_query_string , PHP_URL_QUERY ) , $params );
     $savestate = $params["s"];
     $savestate_img_filename = "previews/".$savestate.".png";
