7ea963a9f4688a94e220f6f742ebd635d5ce4f98
Oliver Motz <services@olivermotz.com>
2023-05-09 20:58:40 +0200

    Format webpack-config

--- webpack/config.js
+++ webpack/config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
 module.exports = {
-  entry: './webpack/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '../export/main/js/compiled/'),
-  },
-  resolve: {
-  alias: {
-    'safe-email': path.resolve(__dirname, '../node_modules/safe-email/dist/safe-email.js')
-  }
-  },
+    entry: './webpack/index.js',
+    output: {
+	filename: 'bundle.js',
+	path: path.resolve(__dirname, '../export/main/js/compiled/'),
+    },
+    resolve: {
+	alias: {
+	    'safe-email': path.resolve(__dirname, '../node_modules/safe-email/dist/safe-email.js')
+	}
+    },
 };
